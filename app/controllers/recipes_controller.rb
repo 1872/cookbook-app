38ae9d55667fdb8142614bae5be9714dabeb27e8
Peter Jang <peter.x.jang@gmail.com>
2016-03-09 21:20:12 -0600

    added show action

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,4 +3,10 @@ class RecipesController < ApplicationController
     @recipes = Recipe.all
     render 'index.html.erb'
   end
+
+  def show
+    recipe_id = params[:id]
+    @recipe = Recipe.find_by(id: recipe_id)
+    render 'show.html.erb'
+  end
 end
