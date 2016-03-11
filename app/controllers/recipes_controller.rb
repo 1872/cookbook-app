7bbec0adef40005e9006e8c866021c4334002574
Peter Jang <peter.x.jang@gmail.com>
2016-03-10 19:38:43 -0600

    added create action with links to index and new pages

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,7 +7,17 @@ class RecipesController < ApplicationController
   def new
     render 'new.html.erb'
   end
-  
+
+  def create
+    Recipe.create(
+      title: params[:title],
+      chef: params[:chef],
+      ingredients: params[:ingredients],
+      directions: params[:directions]
+    )
+    render 'create.html.erb'
+  end
+
   def show
     recipe_id = params[:id]
     @recipe = Recipe.find_by(id: recipe_id)
