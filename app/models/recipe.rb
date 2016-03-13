0e8bd16c907a68948e75a91568a657d4516b8b94
Peter Jang <peter.x.jang@gmail.com>
2016-03-13 16:13:46 -0500

    used model methods for logic, styled index page

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,13 @@
 class Recipe < ActiveRecord::Base
+  def ingredients_list
+    ingredients.split(', ')
+  end
+
+  def directions_list
+    directions.split('. ')
+  end
+
+  def friendly_updated_at
+    updated_at.strftime("%b %e, %l:%M %p")
+  end
 end
